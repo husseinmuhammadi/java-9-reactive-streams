5458040e73d4ea4632ef221087864b12d48c10a9
Hossein Mohammadi <hossein.mohammadi@outlook.com>
2021-01-06 19:15:25 +0330

    Java 9 Reactive Streams

--- document-finder/src/main/java/com/javastudio/tutorial/Application.java
+++ document-finder/src/main/java/com/javastudio/tutorial/Application.java
@@ -1,7 +1,12 @@
 package com.javastudio.tutorial;
 
+import com.javastudio.tutorial.reactive.BookFinder;
+
+import java.awt.print.Book;
+
 public class Application {
     public static void main(String[] args) {
-
+        BookFinder  bookFinder=new BookFinder();
+        bookFinder.start("C:\\");
     }
 }
